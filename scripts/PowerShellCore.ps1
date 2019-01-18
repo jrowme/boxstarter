a53b9b00675f99ed3e7e50aaf1530049c9eda417
hhhensley <hhhensley@gmail.com>
2019-01-18 13:14:15 -0500

    Add PS console color theme tool

--- scripts/PowerShellCore.ps1
+++ scripts/PowerShellCore.ps1
@@ -1,3 +1,6 @@
 choco install -y dotnetcore-sdk
 choco install -y powershell-core
+choco install -y colortool
+(Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/dracula/iterm/m
+aster/Dracula.itermcolors').Content | Out-File -FilePath 'C:\ProgramData\Chocolatey\lib\colortool\content\schemes\Dracula.itermcolors'
 RefreshEnv
