efa56cd4d8d52437712f8b0a72f7393149790212
hhhensley <hhhensley@gmail.com>
2019-01-18 15:12:43 -0500

    Enable Boxstarter OneClick Install

--- henih2-dev-box.ps1
+++ henih2-dev-box.ps1
@@ -7,20 +7,20 @@ Disable-UAC
 $ConfirmPreference = "None" #ensure installing powershell modules don't prompt on needed dependencies
 
 # Get the base URI path from the ScriptToCall value
-# $bstrappackage = "-bootstrapPackage"
-# $helperUri = $Boxstarter['ScriptToCall']
-# $strpos = $helperUri.IndexOf($bstrappackage)
-# $helperUri = $helperUri.Substring($strpos + $bstrappackage.Length)
-# $helperUri = $helperUri.TrimStart("'", " ")
-# $helperUri = $helperUri.TrimEnd("'", " ")
-# $helperUri = $helperUri.Substring(0, $helperUri.LastIndexOf("/"))
-# $helperUri += "/scripts"
-# write-host "helper script base URI is $helperUri"
+$bstrappackage = "-bootstrapPackage"
+$helperUri = $Boxstarter['ScriptToCall']
+$strpos = $helperUri.IndexOf($bstrappackage)
+$helperUri = $helperUri.Substring($strpos + $bstrappackage.Length)
+$helperUri = $helperUri.TrimStart("'", " ")
+$helperUri = $helperUri.TrimEnd("'", " ")
+$helperUri = $helperUri.Substring(0, $helperUri.LastIndexOf("/"))
+$helperUri += "/scripts"
+write-host "helper script base URI is $helperUri"
 
 function executeScript {
   Param ([string]$script)
-  write-host "executing $script ..."
-  Invoke-Expression "scripts\$script"
+  write-host "executing $helperUri/$script ..."
+  iex ((new-object net.webclient).DownloadString("$helperUri/$script"))
 }
 
 #--- Setting up Windows ---
