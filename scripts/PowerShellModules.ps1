b6241cad013d2e0ea5ec2405153b7b38d62d520a
hhhensley <hhhensley@gmail.com>
2019-01-18 19:15:59 -0500

    Add posh-docker module, specify pester install to skip publisher check

--- scripts/PowerShellModules.ps1
+++ scripts/PowerShellModules.ps1
@@ -1,8 +1,9 @@
 Install-Module psake -Force
-Install-Module pester -Force
+Install-Module pester -Force -SkipPublisherCheck
 Install-Module PSLogging -Force
 Install-Module PackageManagement -Force
 Install-Module PowerShellGet -Force
 Install-Module PSWindowsUpdate -Force
 Install-Module PendingReboot -Force
 Install-Module PSScriptAnalyzer -Force
+Install-Module Posh-Docker -Force
