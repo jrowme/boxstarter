81156892dfe120fd13e4ba494b968fdc39383a60
hhhensley <hhhensley@gmail.com>
2019-01-19 16:22:45 -0500

    Specify versions in communication install script

--- scripts/Communication.ps1
+++ scripts/Communication.ps1
@@ -1,5 +1,5 @@
-choco install -y hipchat
-choco install -y slack
-choco install -y microsoft-teams
-choco install -y signal
-choco install -y gitter
+choco install -y hipchat --version=4.30.1.1663
+choco install -y slack --version=3.3.1
+choco install -y microsoft-teams --version=1.1.00.29068
+choco install -y signal --version=1.19.0
+choco install -y gitter --version=4.0.0
