3dbeb3dfce65e3fc56a2535144839dd700d40cc4
hhhensley <hhhensley@gmail.com>
2019-01-20 21:16:07 -0500

    Disable script to remove default apps

--- full-dev-box-setup.ps1
+++ full-dev-box-setup.ps1
@@ -6,21 +6,26 @@ Set-MpPreference -DisableRealtimeMonitoring $true
 
 #--- Browsers ---
 choco install -y googlechrome firefox
+RefreshEnv
 
 # --- Chef Tools ---
 choco install -y chefdk
+RefreshEnv
 
 # --- Dev Tools ---
 choco install -y vscode
 choco install -y git --package-parameters="'/GitAndUnixToolsOnPath /WindowsTerminal'"
+RefreshEnv
 choco install -y python 7zip.install sysinternals f.lux bleachbit dropbox windirstat
 
 # --- Communication Tools ---
 choco install -y hipchat slack microsoft-teams signal gitter
+RefreshEnv
 
 # --- Docker Tools ---
 Enable-WindowsOptionalFeature -Online -FeatureName containers -All
 choco install -y docker-for-windows
+RefreshEnv
 
 #--- Configuring Windows properties ---
 #--- Windows Features ---
@@ -36,19 +41,24 @@ Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name LaunchTo -Value 1
 #taskbar where window is open for multi-monitor
 Set-ItemProperty -Path HKCU:\Software\Microsoft\Windows\CurrentVersion\Explorer\Advanced -Name MMTaskbarMode -Value 2
+RefreshEnv
 
 # --- HashiCorp Tools ---
 choco install -y packer vagrant
+RefreshEnv
 
 # --- HyperV ---
 choco install Microsoft-Hyper-V-All --source="'WindowsFeatures'"
 
 # --- Media Tools ---
 choco install -y vlc spotify
+RefreshEnv
 
 # --- PowerShell Tools ---
 choco install -y dotnetcore-sdk
+RefreshEnv
 choco install -y powershell-core
+RefreshEnv
 choco install -y colortool
 (Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/dracula/iterm/master/Dracula.itermcolors').Content | Out-File -FilePath 'C:\ProgramData\Chocolatey\lib\colortool\content\schemes\Dracula.itermcolors'
 
@@ -65,10 +75,11 @@ Install-Module Posh-Docker -Force
 
 # --- Puppet Tools ---
 choco install -y pdk puppet-bolt
+RefreshEnv
 
 # --- Remove Default Apps ---
 #--- Uninstall unecessary applications that come with Windows out of the box ---
-Write-Host "Uninstall some applications that come with Windows out of the box" -ForegroundColor "Yellow"
+# Write-Host "Uninstall some applications that come with Windows out of the box" -ForegroundColor "Yellow"
 
 #Referenced to build script
 # https://docs.microsoft.com/en-us/windows/application-management/remove-provisioned-apps-during-update
@@ -77,66 +88,68 @@ Write-Host "Uninstall some applications that come with Windows out of the box" -
 # https://gist.github.com/alirobe/7f3b34ad89a159e6daa1
 # https://github.com/W4RH4WK/Debloat-Windows-10/blob/master/scripts/remove-default-apps.ps1
 
-function removeApp {
-  Param ([string]$appName)
-  Write-Output "Trying to remove $appName"
-  Get-AppxPackage $appName -AllUsers | Remove-AppxPackage
-  Get-AppXProvisionedPackage -Online | Where DisplayNam -like $appName | Remove-AppxProvisionedPackage -Online
-}
+# function removeApp {
+#   Param ([string]$appName)
+#   Write-Output "Trying to remove $appName"
+#   Get-AppxPackage $appName -AllUsers | Remove-AppxPackage
+#   Get-AppXProvisionedPackage -Online | Where DisplayNam -like $appName | Remove-AppxProvisionedPackage -Online
+# }
 
-$applicationList = @(
-  "Microsoft.BingFinance"
-  "Microsoft.3DBuilder"
-  "Microsoft.BingFinance"
-  "Microsoft.BingNews"
-  "Microsoft.BingSports"
-  "Microsoft.BingWeather"
-  "Microsoft.CommsPhone"
-  "Microsoft.Getstarted"
-  "Microsoft.WindowsMaps"
-  "*MarchofEmpires*"
-  "Microsoft.GetHelp"
-  "Microsoft.Messaging"
-  "*Minecraft*"
-  "Microsoft.MicrosoftOfficeHub"
-  "Microsoft.OneConnect"
-  "Microsoft.WindowsPhone"
-  "Microsoft.WindowsSoundRecorder"
-  "*Solitaire*"
-  "Microsoft.MicrosoftStickyNotes"
-  "Microsoft.Office.Sway"
-  "Microsoft.XboxApp"
-  "Microsoft.XboxIdentityProvider"
-  "Microsoft.ZuneMusic"
-  "Microsoft.ZuneVideo"
-  "Microsoft.NetworkSpeedTest"
-  "Microsoft.FreshPaint"
-  "Microsoft.Print3D"
-  "*Autodesk*"
-  "*BubbleWitch*"
-  "king.com*"
-  "G5*"
-  "*Dell*"
-  "*Facebook*"
-  "*Keeper*"
-  "*Netflix*"
-  "*Twitter*"
-  "*Plex*"
-  "*.Duolingo-LearnLanguagesforFree"
-  "*.EclipseManager"
-  "ActiproSoftwareLLC.562882FEEB491" # Code Writer
-  "*.AdobePhotoshopExpress"
-);
+# $applicationList = @(
+#   "Microsoft.BingFinance"
+#   "Microsoft.3DBuilder"
+#   "Microsoft.BingFinance"
+#   "Microsoft.BingNews"
+#   "Microsoft.BingSports"
+#   "Microsoft.BingWeather"
+#   "Microsoft.CommsPhone"
+#   "Microsoft.Getstarted"
+#   "Microsoft.WindowsMaps"
+#   "*MarchofEmpires*"
+#   "Microsoft.GetHelp"
+#   "Microsoft.Messaging"
+#   "*Minecraft*"
+#   "Microsoft.MicrosoftOfficeHub"
+#   "Microsoft.OneConnect"
+#   "Microsoft.WindowsPhone"
+#   "Microsoft.WindowsSoundRecorder"
+#   "*Solitaire*"
+#   "Microsoft.MicrosoftStickyNotes"
+#   "Microsoft.Office.Sway"
+#   "Microsoft.XboxApp"
+#   "Microsoft.XboxIdentityProvider"
+#   "Microsoft.ZuneMusic"
+#   "Microsoft.ZuneVideo"
+#   "Microsoft.NetworkSpeedTest"
+#   "Microsoft.FreshPaint"
+#   "Microsoft.Print3D"
+#   "*Autodesk*"
+#   "*BubbleWitch*"
+#   "king.com*"
+#   "G5*"
+#   "*Dell*"
+#   "*Facebook*"
+#   "*Keeper*"
+#   "*Netflix*"
+#   "*Twitter*"
+#   "*Plex*"
+#   "*.Duolingo-LearnLanguagesforFree"
+#   "*.EclipseManager"
+#   "ActiproSoftwareLLC.562882FEEB491" # Code Writer
+#   "*.AdobePhotoshopExpress"
+# );
 
-foreach ($app in $applicationList) {
-  removeApp $app
-}
+# foreach ($app in $applicationList) {
+#   removeApp $app
+# }
 
 # --- RSAT Tools ---
 Get-WindowsCapability -Online -Name rsat* | Add-WindowsCapability -Online
+RefreshEnv
 
 # --- Windows Subsystem Linux ---
 choco install Microsoft-Windows-Subsystem-Linux --source="'windowsfeatures'"
+RefreshEnv
 
 #--- Ubuntu ---
 # TODO: Move this to choco install once --root is included in that package
