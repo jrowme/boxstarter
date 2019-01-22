996fb91be7183fe980548a28322341f2d2be4f33
hhhensley <hhhensley@gmail.com>
2019-01-22 09:38:41 -0500

    Set console theme using colortool

--- full-dev-box-setup.ps1
+++ full-dev-box-setup.ps1
@@ -61,6 +61,8 @@ choco install -y powershell-core
 RefreshEnv
 choco install -y colortool
 (Invoke-WebRequest -UseBasicParsing 'https://raw.githubusercontent.com/dracula/iterm/master/Dracula.itermcolors').Content | Out-File -FilePath 'C:\ProgramData\Chocolatey\lib\colortool\content\schemes\Dracula.itermcolors'
+RefreshEnv
+ColorTool.exe Dracula.itermcolors
 
 # --- PowerShell Modules ---
 Install-Module psake -Force
